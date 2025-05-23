4e23985f0129154a7e2e7023f4b85d6aa676729b
AabhashSoni01 <aabhashsoni2@gmail.com>
2025-05-23 11:40:08 +0530

    arrayList

---
+++ ArrayListQue2.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class ArrayListQue2 {
+
+    // max subrray sum
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(1);
+        arr.add(-2);
+        arr.add(3);
+        arr.add(4);
+        arr.add(5);
+        arr.add(-6);
+    }
+}
