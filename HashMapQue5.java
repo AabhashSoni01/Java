a971ada5dc94bb3ce4a3c26bc2ac290a04012d0e
AabhashSoni01 <aabhashsoni2@gmail.com>
2025-05-30 03:11:01 +0530

    hashMap

--- HashMapQue5.java
+++ HashMapQue5.java
@@ -1,3 +1,23 @@
+import java.util.*;
+
 public class HashMapQue5 {
-    
+
+    // Sort a Map by Keys and Values
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("Banana", 10);
+        map.put("Apple", 30);
+        map.put("Mango", 20);
+
+        Map<String, Integer> treeMap = new TreeMap<>(map);
+        System.out.println("Sorted by keys : " + treeMap);
+
+        List<Map.Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
+        list.sort(Map.Entry.comparingByValue());
+
+        System.out.println("Sorted by values : ");
+        for (Map.Entry<String, Integer> entry : list) {
+            System.out.println(entry.getKey() + " => " + entry.getValue());
+        }
+    }
 }
