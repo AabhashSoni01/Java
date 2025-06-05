c7ccdfa594d84ae6bb5fd3e917626f226def7c44
AabhashSoni01 <aabhashsoni2@gmail.com>
2025-06-05 14:04:02 +0530

    multithreading

---
+++ Multithreading5.java
@@ -0,0 +1,43 @@
+public class Multithreading5 {
+
+    public static class simpleWaitNotify {
+        private boolean isSignalled = false;
+
+        public synchronized void sendSignal() {
+            isSignalled = true;
+            System.out.println("Producer: Sending signal...");
+            notify();
+        }
+
+        public synchronized void waitForSignal() {
+            while (!isSignalled) {
+                try {
+                    System.out.println("Consumer: Waiting for signal...");
+                    wait();
+                } catch (InterruptedException e) {
+
+                }
+            }
+            System.out.println("Consumer: Got the signal! Proceeding...");
+        }
+    }
+
+    public static void main(String[] args) {
+        simpleWaitNotify obj = new simpleWaitNotify();
+
+        Thread consumer = new Thread(() -> {
+            obj.waitForSignal();
+        });
+
+        Thread producer = new Thread(() -> {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+            }
+            obj.sendSignal();
+        });
+
+        consumer.start();
+        producer.start();
+    }
+}
