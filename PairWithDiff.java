3e962bda795b16daef3d5a6f7e9a38b0203ebae7
AabhashSoni01 <aabhashsoni2@gmail.com>
2025-06-21 23:51:07 +0530

    PairWithDiff

---
+++ PairWithDiff.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class PairWithDiff {
+    public static boolean hasPairWithDiff(int[] arr, int k) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : arr) {
+            if (set.contains(num + k) || set.contains(num - k))
+                return true;
+            set.add(num);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 5, 20, 3, 2, 50, 80 };
+        System.out.println(hasPairWithDiff(arr, 78)); // true
+    }
+}
